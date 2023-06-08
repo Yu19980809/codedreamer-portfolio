dfd80e6479a2f60e2af5e27a9b59f3d8d3cfd8ff
Yu19980809 <1572250276@qq.com>
2023-06-08 17:53:26 +0800

    finished contact us section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,11 @@
 import logo from './logo.png';
+import address from './address.png';
+import phone from './phone.png';
+import mail from './mail.png';
 
 export {
-  logo
+  logo,
+  address,
+  phone,
+  mail
 };
