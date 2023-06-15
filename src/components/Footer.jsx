3e0754af0283a7421165a274108fc5440e27d833
Yu19980809 <1572250276@qq.com>
2023-06-15 18:14:09 +0800

    set responsive configuration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
 import { socialMedia } from '../constants';
+import { styles } from '../styles';
 
 const Footer = () => (
-  <footer className="flex justify-between items-center w-full h-[80px] mt-[80px] border-t border-t-dimWhite">
-    <p className="text-dimWhite text-[18px]">2023 CodeDreamer All Rights Reserved.</p>
+  <footer className={ `${ styles.paddingX }` }>
+    <div className="flex justify-between items-center w-full h-[80px] mt-[80px] border-t border-t-dimWhite text-dimWhite">
+      <p className="md:text-[18px] text-[16px]">2023 CodeDreamer All Rights Reserved.</p>
 
-    <div className="flex gap-8">
-      { socialMedia.map( media => (
-        <a
-          key={ media.id }
-          href={ media.link }
-          rel="noreferrer"
-          target="_blank" 
-          className="w-[24px] h-[24px]"
-        >
-          <img src={ media.icon } alt={ media.name } className="w-full h-full object-contain" />
-        </a>
-      ) ) }
+      <div className="flex gap-8">
+        { socialMedia.map( media => (
+          <a
+            key={ media.id }
+            href={ media.link }
+            rel="noreferrer"
+            target="_blank" 
+            className={ `iconfont md:text-[18px] text-[16px] ${ media.fontClass }` }
+          />
+        ) ) }
+      </div>
     </div>
   </footer>
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { logo, menu, close } from '../assets';
+import { logo, menu, close } from '../assets/images';
 import { navbarLinks } from '../constants';
+import { styles } from '../styles';
 
 const Navbar = () => {
   const [ currentSection, setCurrentSection ] = useState( 'Home' );
@@ -10,31 +11,31 @@ const Navbar = () => {
   useEffect( () => {
     window.addEventListener( 'scroll', () => {
       const scrollY = window.scrollY;
-      if ( scrollY > 80 ) setIsNavbarFixed( true );
+      ( scrollY > 80 ) ?  setIsNavbarFixed( true ) : setIsNavbarFixed( false );
 
-      if ( scrollY <= 80 ) {
-        setCurrentSection( 'Home' )
-      } else if ( scrollY <= 100 ) {
-        setCurrentSection( 'Services' )
-      } else if ( scrollY <= 120 ) {
-        setCurrentSection( 'Works' )
-      }
+      // if ( scrollY <= 80 ) {
+      //   setCurrentSection( 'Home' )
+      // } else if ( scrollY <= 100 ) {
+      //   setCurrentSection( 'Services' )
+      // } else if ( scrollY <= 120 ) {
+      //   setCurrentSection( 'Works' )
+      // }
     } );
   } );
 
   return (
-    <nav className={ `${ isNavbarFixed ? 'fixed top-0 bg-black-900 container mx-auto' : '' } w-full z-10` }>
+    <nav className={ `${ styles.paddingX } ${ isNavbarFixed ? 'fixed top-0 bg-black-900' : '' } w-full max-w-[1536px] z-10` }>
       <div className="flex justify-between items-center w-full h-[80px]">
         {/* logo */}
         <div className="flex items-center">
-          <img src={ logo } alt="logo" className="w-[64px] h-[64px]" />
+          <img src={ logo } alt="logo" className="w-[64px] h-[64px] mx-[-16px]" />
           <p className="text-[20px] font-bold">
             Code<span className="text-primary">Dreamer</span>
           </p>
         </div>
 
         {/* links */}
-        <ul className="hidden sm:flex items-center gap-5 list-none">
+        <ul className="hidden md:flex items-center lg:gap-10 gap-5 list-none">
           { navbarLinks.map( link => (
             <li
               key={ link.name }
@@ -46,8 +47,8 @@ const Navbar = () => {
           ) ) }
         </ul>
         
-        {/* links on small screen */}
-        <div className="sm:hidden flex flex-col justify-end items-center gap-5">
+        {/* links on small and midium screen */}
+        <div className="md:hidden flex flex-col justify-end items-center gap-5">
           <img
             src={ isOpen ? close : menu }
             alt="menu"
@@ -61,7 +62,10 @@ const Navbar = () => {
                 <li
                   key={ link.name }
                   className={ `${ ( link.name === currentSection ) ? 'text-primary border-b border-b-2 border-b-primary' : '' } text-[18px] font-bold cursor-pointer` }
-                  onClick={ () => setCurrentSection( link.name ) }
+                  onClick={ () => {
+                    setCurrentSection( link.name );
+                    setIsOpen( false );
+                  } }
                 >
                   <a href={ `#${ link.name }` }>{ link.name }</a>
                 </li>
