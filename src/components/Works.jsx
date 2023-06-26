09e8766625f3824de3925480b37560348087e80c
Corrine He <corrinegogoup@gmail.com>
2023-06-26 22:31:46 +0800

    update work image and JS

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,13 +14,18 @@ const Works = () => {
         {/* every single card */}
         {works.works.map((work) => (
           <div
-            className="pl-[26px] group rounded-3xl bg-work-gradient text-white/70 border-2 border-transparent hover:bg-slate-200/50 hover:text-white hover:border-2 hover:border-borderGrey shadow-xl shadow-blue-500/50:;
-          "
+            className={`pl-[26px] group rounded-3xl text-white/70 border-2 border-transparent hover:bg-slate-200/50 hover:text-white hover:border-2 hover:border-borderGrey  shadow-xl shadow-blue-500/50:;
+            ${activeWork === work.name ? "bg-slate-200/50 text-white border-2 border-borderGrey" : "bg-work-gradient" }`
+          }
             key={work.name}
             onClick={() => {setActiveWork(work.name)}}
           >
             <div className="work-title pt-[35px] pb-[16.9px]">{work.name}</div>
-            <div className="text-[14px]">{work.description}</div>
+
+            { activeWork === work.name && (
+              <div className="text-[14px]">{work.description}</div>
+            ) }
+
             <div className="flex pb-[30px] pt-[13px] flex space-x-[14px]">
               {work.tags.map((tag) => (
                 <div
@@ -37,30 +42,63 @@ const Works = () => {
       </div>
 
       {/* --------------------- right image card --------------------- */}
-      <div className="flex w-[800px]  h-[666px]  border relative">
+      <div className="flex w-[800px]  h-[666px] relative">
         {/* bg text */}
-        <div className="flex w-[582px] h-[225px] flex-col justify-start border">
-          <div className="text-textPurple text-[150px] font-semibold">
-            <div className="absolute top-[10px] left-[35px]">TripTirp</div>
-            <div className="absolute bottom-[242.23px] left-[93.61px]">
-              TripTirp
+        <div className="flex w-[582px] h-[225px] flex-col justify-start">
+
+          {activeWork === "Hobos" && (
+            <div className="text-textPurple text-[150px] font-semibold">
+              <div className="absolute top-[10px] left-[35px]">Hobos</div>
+              <div className="absolute bottom-[242.23px] left-[93.61px]">
+              Hobos
+              </div>
+              <div className="absolute bottom-[38.8px] left-[188.43px]">
+                Hobos
+              </div>
             </div>
-            <div className="absolute bottom-[38.8px] left-[188.43px]">
-              TripTirp
+          )}
+
+          {activeWork === "OE Biotech" && (
+            <div className="text-textPurple text-[150px] font-semibold">
+              <div className="absolute top-[10px] left-[35px]">OE Biotech</div>
+              <div className="absolute bottom-[242.23px] left-[93.61px]">
+              OE Biotech
+              </div>
+              <div className="absolute bottom-[38.8px] left-[188.43px]">
+                OE Biotech
+              </div>
             </div>
-          </div>
+          )}
+
+          {activeWork === "Triptrip" && (
+
+            <div className="text-textPurple text-[150px] font-semibold">
+              <div className="absolute top-[10px] left-[35px]">TripTirp</div>
+              <div className="absolute bottom-[242.23px] left-[93.61px]">
+              TripTirp
+              </div>
+              <div className="absolute bottom-[38.8px] left-[188.43px]">
+                TripTirp
+              </div>
+            </div>
+          )}
+
+
+
         </div>
 
         {/* images */}
         {activeWork === "Hobos" && (
           <img
-            className="absolute bottom-[274px] left-[96px]"
+            className="absolute w-28 top-[10px] left-[0px] rounded-full"
             src="/src/assets/images/Hobos-QR.jpeg"
             alt=""
           />
         )}
         {activeWork === "Triptrip" && (
           <div>
+
+            <img className="absolute w-28 top-[10px] left-[0px] rounded-full" src="/src/assets/images/triptrip-QR.jpeg" alt="" />
             <img
               className="absolute bottom-[84.41px] left-[96px]"
               src="/src/assets/images/triptrip1.png"
