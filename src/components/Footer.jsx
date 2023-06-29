0ccfeff9a6a50be67d9e862f9fb72089769791c0
JiaoyangDong <zoradongccc@gmail.com>
2023-06-29 15:25:22 +0800

    adjust Footer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import { socialMedia } from '../constants';
+// import { socialMedia } from '../constants';
 import { styles } from '../styles';
 
 const Footer = () => (
   <footer className={ `${ styles.paddingX }` }>
     <div className="flex justify-between items-center w-full h-[80px] mt-[80px] border-t border-t-dimWhite text-dimWhite">
-      <p className="md:text-[18px] text-[16px]">2023 CodeDreamer All Rights Reserved.</p>
+      <p className="md:text-base text-xs">2023 CodeDreamer All Rights Reserved.</p>
 
-      <div className="flex gap-8">
-        { socialMedia.map( media => (
+      <div className="flex gap-6">
+        {/* { socialMedia.map( media => (
           <a
             key={ media.id }
             href={ media.link }
             rel="noreferrer"
-            target="_blank" 
+            target="_blank"
             className={ `iconfont md:text-[18px] text-[16px] ${ media.fontClass }` }
           />
-        ) ) }
+        ) ) } */}
       </div>
     </div>
   </footer>
